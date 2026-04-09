f997a7a189864a9d7ca01ecd726dfa9239c368b6
Kangle Jiang <saguorn@gmail.com>
2026-04-09 19:07:19 +0800

    added create new note form

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.note_list, name='note_list'),
+    path('new/', views.note_create, name='note_create'), # Add this
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
+from .forms import NoteForm
 
+# Updated List View
 def note_list(request):
-    # This grabs all notes from the database
     notes = Note.objects.all().order_by('-updated_at')
-    
-    # This sends those notes to an HTML file (which we will create next)
     return render(request, 'notes/note_list.html', {'notes': notes})
+
+# New Create View
+def note_create(request):
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            # For now, we manually assign the first user (you) 
+            # until we set up the Login system next.
+            from django.contrib.auth.models import User
+            note.user = User.objects.first() 
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/note_form.html', {'form': form})
